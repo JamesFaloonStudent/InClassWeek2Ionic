7c408360594e74cd37fe77cf90ce84e44c6796ed
Your Name <you@example.com>
2025-05-12 11:42:57 -0400

    Component Imports

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { C1Component } from './c1/c1.component'
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    IonicModule,
+    FormsModule
+  ],
+  export : [C1Component]
 })
 export class ComponentsModule { }
