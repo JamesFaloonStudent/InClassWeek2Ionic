3876ee52738726dc309b79739ce8dd4adb86a318
Your Name <you@example.com>
2025-05-12 15:20:05 -0400

    added components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { C1Component } from './c1/c1.component'
-
+import { C2Component } from './c2/c2.component'
+import { C3Component } from './c3/c3.component'
 
 @NgModule({
   declarations: [],
@@ -11,8 +12,10 @@ import { C1Component } from './c1/c1.component'
     CommonModule,
     IonicModule,
     FormsModule,
-    C1Component
+    C1Component,
+    C2Component,
+    C3Component
   ],
-  exports : [C1Component]
+  exports : [C1Component, C2Component, C3Component]
 })
 export class ComponentsModule { }
